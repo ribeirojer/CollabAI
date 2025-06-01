0a4069f0c4fc917d38e1086929fcc5909c460092
José Eduardo Ribeiro <eduardojerbr@gmail.com>
2025-06-01 00:03:25 -0300

    feat: create interfaces

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,14 +4,14 @@ const { NEXT_PUBLIC_SUPABASE_URL } = process.env;
 const { NEXT_PUBLIC_SUPABASE_ANON_KEY } = process.env;
 
 if (!NEXT_PUBLIC_SUPABASE_URL) {
-  throw new Error('NEXT_PUBLIC_Supabase URL must be defined in environment variables');
+  console.log('NEXT_PUBLIC_Supabase URL must be defined in environment variables');
 }
 
 if (!NEXT_PUBLIC_SUPABASE_ANON_KEY) {
-  throw new Error('NEXT_PUBLIC_Supabase Anon Key must be defined in environment variables');
+  console.log('NEXT_PUBLIC_Supabase Anon Key must be defined in environment variables');
 }
 
 export const supabase = createClient(
-  NEXT_PUBLIC_SUPABASE_URL,
-  NEXT_PUBLIC_SUPABASE_ANON_KEY
+  NEXT_PUBLIC_SUPABASE_URL as string,
+  NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 );
